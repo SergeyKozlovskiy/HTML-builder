fdc6d3058436a8f90498e05fe41394d76d9c6c97
Sergey <zavulon31990@gmail.com>
2021-11-04 13:07:48 +0300

    feat: complete tasks 1,2

---
+++ 02-write-file/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const filePath = `${path.join(__dirname, "data.txt")}`;
+
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  fs.writeFile(
+    filePath,
+    '',
+    'utf8',
+    (err) => {
+      if (err) throw err;
+      console.log('Hello, enter text or "exit" to exit');
+    }
+  );
+
+  rl.on('line', (line) => {
+    if(line !== 'exit'){
+        fs.appendFile(
+            filePath,
+            `\n${line}`,
+            'utf8',
+            (err) => {
+              if (err) throw err;
+            }
+          );
+    }else{
+        console.log('Good Bye! Take care!');
+        rl.close();
+    }
+  });  
+
+  rl.on('SIGINT', () => {
+    console.log('Good Bye! Take care!');
+    rl.close();
+  });
