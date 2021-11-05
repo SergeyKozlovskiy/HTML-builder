9fb503d3d0b79f441d8b2956351720498f9dc4fc
Sergey <zavulon31990@gmail.com>
2021-11-05 13:50:39 +0300

    feat: complete task 4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,15 +10,14 @@ fs.readdir(filePath, {withFileTypes: true}, (err, files) => {
     files.forEach(el => {
         if(el.isFile()){
             let name = path.parse(el.name).name;
-            let ext = path.parse(el.name).ext;
+            let ext = path.parse(el.name).ext.substr(1);
             let pathEl = `${filePath}/${el.name}`;
             fs.stat(pathEl, (error, stats) => {
                 if (error) {
                   console.log(error);
                 }
                 else {
-                  let size = Math.round(stats.size/1024);
-                  console.log('\x1b[36m%s\x1b[0m', `${name} ${ext} ${size}kb`);
+                  console.log('\x1b[36m%s\x1b[0m', `${name} - ${ext} - ${stats.size}`);
                 }
             });
         }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const copyFilesPath = `${__dirname}/files-copy`;
+const filesPath = `${__dirname}/files`;
+
+fs.stat(copyFilesPath, (err, stats) => {
+// если не существует создать
+  if (err) {
+    fs.mkdir(copyFilesPath, { recursive: true }, (err) => {
+    if (err) throw err;
+    });
+  }
+// если существует удалить содержимое
+    fs.readdir(copyFilesPath, {withFileTypes: false}, (err, files) => {
+        if (err) throw err;
+        files.forEach(el => {
+            fs.unlink(`${copyFilesPath}/${el}`, (err) => {
+                if (err) throw err;
+            });
+        })
+    })
+// и скопировать файлы
+    fs.readdir(filesPath, {withFileTypes: false}, (err, files) => {
+        if (err) throw err;
+        files.forEach(el => {
+            fs.copyFile(`${filesPath}/${el}`, `${copyFilesPath}/${el}`, err => {
+            if(err) throw err; 
+            });
+        })
+    });
+})
+
+
 
